c6cbf43395f3df2d38c85f88d55a551a6c02aa0c
hydd <10320073+hydd@users.noreply.github.com>
2019-02-24 13:20:42 +0800

    ss订阅转clash_beta3

--- SS_clash(from ss subscription)/SS_clash.py
+++ SS_clash(from ss subscription)/SS_clash.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-# from bs4 import BeautifulSoup
 import requests
 import base64
 
 
-def getBasefile(url):
+def getBasefile(url):  # 获取订阅链接加密文本
     try:
         html = requests.get(url)
         html.raise_for_status
@@ -15,19 +14,20 @@ def getBasefile(url):
         return "错误"
 
 
-def getAllLinks(url):
-    # print(strg)
+def getAllLinks(url):  # 从加密文本解析出所有ss链接
     links = getBasefile(url)
     result = decodeInfo(links)
     alllinks = result.split('\\n')
+    if len(alllinks[-1]) < 10:
+        alllinks.pop()
     return alllinks
 
 
-def getAllNodes(url):
+def getAllNodes(url):  # 从ss链接汇总得到所有节点信息
     allnodes = []
     links = getAllLinks(url)
     for ss in links:
-        link = formatLink(ss.split('//')[1].split("'")[0])
+        link = ss.split('//')[1].split("'")[0]
         # node = getNode(link) if ss.split(':')[0] == "ss" else getNodeR(link)
         if ss.split(':')[0] == "ss":
             node = getNode(link)
@@ -42,13 +42,13 @@ def getAllNodes(url):
     return allnodes
 
 
-def formatLink(link):
-    l1 = link.replace('-', '+')
-    l2 = l1.replace('_', '/')
-    return l2
+# def formatLink(link):
+#     l1 = link.replace('-', '+')
+#     l2 = l1.replace('_', '/')
+#     return l2
 
 
-def getNode(link):
+def getNode(link):  # 从ss链接中得到节点信息
     info = decodeInfo(link)
     method = info.split(':')[0]
     pwd = info.split("@")[0].split(":")[1]
@@ -59,29 +59,35 @@ def getNode(link):
     return node
 
 
-def getNodeR(link):
+def getNodeR(link):  # 从ssr链接中得到节点信息
     info = decodeInfo(link)
-    pwd = decodeInfo(info.split('?')[0].split(':')[-1]).split("'")[1]
+    pwd = decodeInfo(info.split('/')[0].split(':')[-1]).split("'")[1]
     server = info.split(':')[0].split("'")[1]
     port = info.split(':')[1]
     protocol = info.split(':')[2]
     method = info.split(':')[3]
     obfs = info.split(':')[4]
-    remark = getName(formatLink(info.split('&')[2].split('=')[1]))
+    remark = getName(info.split('&')[2].split('=')[1])
 
     # print(server, port, method, pwd, protocol, obfs, remark)
     node = [remark, server, port, method, pwd, protocol, obfs]
     return node
 
 
-def getName(info):
+def getName(info):  # 得到节点名称（有待合并）
     lens = len(info)
-    lenx = lens - (lens % 4 if lens % 4 else 4)
-    result = base64.b64decode(info[:lenx]).decode('utf-8', errors='ignore')
+    # lenx = lens - (lens % 4 if lens % 4 else 4)
+    if lens % 4 == 1:
+        info = info + "==="
+    elif lens % 4 == 2:
+        info = info + "=="
+    elif lens % 4 == 3:
+        info = info + "="
+    result = base64.urlsafe_b64decode(info).decode('utf-8', errors='ignore')
     return result
 
 
-def checkNode(node):
+def checkNode(node):  # 检查节点是否是ss节点
     obfs = node[6]
     pro = node[5]
     if checkObfs(obfs) and checkPro(pro):
@@ -90,32 +96,33 @@ def checkNode(node):
         return False
 
 
-def checkObfs(str):
-    # 检查是否为ss混淆
+def checkObfs(str):  # 检查是否为ss混淆
     if str == "plain" or str.split('_')[-1] == "compatible":
         return True
     else:
         return False
 
 
-def checkPro(str):
-    # 检查是否为ss协议
+def checkPro(str):  # 检查是否为ss协议
     if str == "origin" or str.split('_')[-1] == "compatible":
         return True
     else:
         return False
 
 
-def decodeInfo(info):
+def decodeInfo(info):  # 解码加密内容
     lens = len(info)
-    # print(lens)
-    lenx = lens - (lens % 4 if lens % 4 else 4)
-    result = str(base64.b64decode(info[:lenx]))
+    if lens % 4 == 1:
+        info = info + "==="
+    elif lens % 4 == 2:
+        info = info + "=="
+    elif lens % 4 == 3:
+        info = info + "="
+    result = str(base64.urlsafe_b64decode(info))
     return result
 
 
-def setNodes(nodes):
-    # 设置节点
+def setNodes(nodes):  # 设置节点
     proxies = []
     for node in nodes:
         name = node[0]
@@ -132,12 +139,10 @@ def setNodes(nodes):
     return proxies
 
 
-def setPG(nodes):
-    # 设置策略组 auto,Fallback-auto,Proxy
+def setPG(nodes):  # 设置策略组 auto,Fallback-auto,Proxy
     proxy_names = []
     for node in nodes:
         proxy_names.append(node[0])
-    # print(str(proxy_names))
     auto = "- { name: 'auto', type: url-test, proxies: " + str(
         proxy_names
     ) + ", url: 'http://www.gstatic.com/generate_204', interval: 300 }\n"
@@ -154,8 +159,7 @@ def setPG(nodes):
 
 
 def getClash(nodes):
-    # with open("./General.yml", "r") as f:
-    #     gener = f.read()
+
     gener = getBasefile(
         'https://raw.githubusercontent.com/JRQLS/ToClash/master/General.yml')
     with open("./clash.yml", "w") as f:
@@ -165,8 +169,6 @@ def getClash(nodes):
     with open("./clash.yml", "a") as f:
         f.writelines(info)
 
-    # with open("./rules.yml", "r") as f:
-    #     rules = f.read()
     rules = getBasefile(
         'https://raw.githubusercontent.com/JRQLS/ToClash/master/rules.yml')
     with open("./clash.yml", "a") as f:
@@ -176,6 +178,5 @@ def getClash(nodes):
 if __name__ == "__main__":
     url = "https://jumpc.xyz/link/6fhH5JIjPQkh5O9y"
     nodes = getAllNodes(url)
-    # for node in nodes:
-    #     print(node)
+
     getClash(nodes)
